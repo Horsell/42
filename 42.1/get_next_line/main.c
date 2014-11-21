084fb6dc24e3053ff984bfa2feacbdcf625e0870
jpirsch <jeanpirsch@gmail.com>
2014-11-21 07:50:24 +0100

    complete mess

--- 42.1/get_next_line/main.c
+++ 42.1/get_next_line/main.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpirsch <jpirsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/18 20:55:01 by jpirsch           #+#    #+#             */
-/*   Updated: 2014/11/21 07:29:05 by jpirsch          ###   ########.fr       */
+/*   Created: 2014/11/19 00:27:21 by jpirsch           #+#    #+#             */
+/*   Updated: 2014/11/21 04:54:56 by jpirsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
-# define BUF_SIZE 200
+#include "libft.h"
+#include <fcntl.h>
 
-int	get_next_line(const int fd, char **line);
-#endif
+int	main(void)
+{
+	char	**line;
+	size_t	i;
+	int		fd;
+
+	i = 0;
+	fd = open("ldap", O_RDONLY);
+	get_next_line(fd, line);
+	//while (get_next_line(fd, line))
+	//	ft_putendl(*line);
+	return (0);
+}
