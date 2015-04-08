8a2649ea4ecb1ff6da40aca70358a9f5fcd20db0
jpirsch <jeanpirsch@gmail.com>
2015-04-08 07:59:07 +0200

    env -i and basic execve, working on builtins

---
+++ 42.1/ft_sh1/sources/hashtable.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hashtable.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpirsch <jpirsch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/03 16:30:49 by jpirsch           #+#    #+#             */
+/*   Updated: 2015/04/08 07:34:17 by jpirsch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <dirent.h>
+#include "libft.h"
+#include "ft_sh1.h"
+
+void	hash(t_env *e)
+{
+	int				i;
+	DIR				*dir;
+	char			*cmd_path;
+	struct dirent	*dp;
+	t_list			*new;
+
+	i = 0;
+	while (i < e->ec)
+	{
+		if ((dir = opendir(e->path[i])))
+		{
+			while ((dp = readdir(dir)) != NULL)
+			{
+				if ((ft_strncmp(dp->d_name, "..", 2)) ||
+						(ft_strncmp(dp->d_name, ".", 1)))
+				{
+					cmd_path = ft_strjoin(e->path[i], "/");
+					cmd_path = ft_strjoin(cmd_path, dp->d_name);
+					new = ft_lstnew(cmd_path, ft_strlen(cmd_path));
+					ft_lstadd(&e->hash, new);
+					if (e->hash)
+					{
+			ft_putendl(e->hash->content);
+					}
+				}
+			}
+			closedir(dir);
+		}
+		i++;
+	}
+}
+
+int		check_path(t_env *e)
+{
+	int				i;
+	int				n;
+	char			*path;
+	char			**tab_path;
+	char			*join;
+
+//	hash(e);
+	i = 0;
+	while (i < e->pc)
+	{
+		join = ft_strjoin(e->path[i], "/");
+		join = ft_strjoin(join, e->av[0]);
+		ft_execve(join, e->av, e->env);
+		i++;
+	}
+	return (--i);
+}
