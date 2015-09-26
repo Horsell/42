d5e8d0ab9e312051eed120326f65002f7031f6af
jpirsch <jeanpirsch@gmail.com>
2015-09-26 17:44:33 +0200

    new project

---
+++ 42.1/ft_linear_regression/ft_linear_regression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+from sys import argv
+import matplotlib.pyplot as plt
+import numpy as np
+
+x,y = np.loadtxt("Data.txt", unpack='true')
+
+#plt.scatter(x,y)
+plt.plot(x, y)
+plt.show()
