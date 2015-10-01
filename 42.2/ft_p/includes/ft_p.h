8978b970eea74fd6b40b7cff554383760b9a3383
Jean PIRSCH <jpirsch@e3r8p15.42.fr>
2015-10-01 09:00:05 +0200

    Started project with example from tutorialspoint.com

---
+++ 42.2/ft_p/includes/ft_p.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_p.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpirsch <jpirsch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/01 01:57:41 by jpirsch           #+#    #+#             */
+/*   Updated: 2015/10/01 02:03:13 by jpirsch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_P_H
+# define FT_P_H
+# include <signal.h>
+# include <sys/wait.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include "libft.h"
+
+# define C_NONE "\033[0m"
+# define C_BOLD "\033[1m"
+# define C_BLACK "\033[30m"
+# define C_RED "\033[31m"
+# define C_GREEN "\033[32m"
+# define C_BROWN "\033[33m"
+# define C_BLUE "\033[0;34m"
+# define C_MAGENTA "\033[35m"
+# define C_CYAN "\033[36m"
+# define C_GRAY "\033[37m"
+
+#endif
